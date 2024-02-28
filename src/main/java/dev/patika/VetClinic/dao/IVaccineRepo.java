c9b8b1f531bfb1e00e74e3108862de8eea256f5f
Rockn26 <irfanank26@gmail.com>
2024-02-28 17:50:46 +0300

    fix repo

--- src/main/java/dev/patika/VetClinic/dao/IVaccineRepo.java
+++ src/main/java/dev/patika/VetClinic/dao/IVaccineRepo.java
@@ -17,8 +17,8 @@ public interface IVaccineRepo extends JpaRepository<Vaccine, Long> {
     Optional<Vaccine> findByCodeAndNameAndProtectionFinishDateAfter(String code, String name, LocalDate date);
 
     // Aynı tip aşıların ve bitiş tarihinin kontrolünü yapacak özel sorgu
-    @Query("SELECT v FROM Vaccine v WHERE v.name = :name AND v.code = :code AND v.protectionFinishDate >= :protectionFinishDate")
-    List<Vaccine> findExistingVaccines(@Param("name") String name, @Param("code") String code, @Param("protectionFinishDate") LocalDate protectionFinishDate);
+    @Query("SELECT v FROM Vaccine v WHERE v.name = :name AND v.code = :code AND v.protectionFinishDate >= :protectionStartDate")
+    Optional<Vaccine> checkIfVaccineIsValid(@Param("name") String name, @Param("code") String code, @Param("protectionStartDate") LocalDate protectionStartDate);
 
 
     List<Vaccine> findByAnimalId(Long animalId);
