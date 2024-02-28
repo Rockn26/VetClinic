5fd9fa3a8e29099341cc6baeab5915266d970e5b
Rockn26 <irfanank26@gmail.com>
2024-02-28 18:33:54 +0300

    create report

---
+++ src/main/java/dev/patika/VetClinic/entities/Report.java
@@ -0,0 +1,31 @@
+package dev.patika.VetClinic.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Report {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String diagnosis;
+    private double price;
+
+    @OneToOne
+    private Appointment appointment;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "report", cascade = CascadeType.REMOVE)
+    private List<Vaccine> vaccines;
+
+}
