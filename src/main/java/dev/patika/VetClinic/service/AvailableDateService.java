375d90a3eb743f5e149fd3a0c918c118d9ad88d5
Rockn26 <irfanank26@gmail.com>
2024-02-28 18:35:52 +0300

    add availabledate

--- src/main/java/dev/patika/VetClinic/service/AvailableDateService.java
+++ src/main/java/dev/patika/VetClinic/service/AvailableDateService.java
@@ -11,7 +11,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -66,4 +68,9 @@ public class AvailableDateService {
     public void delete(Long id) {
         availableDateRepo.delete(getById(id));
     }
+
+    public Optional<AvailableDate> getByAvailableDate(LocalDate availableDate) {
+        return availableDateRepo.findByAvailableDate(availableDate);
+    }
+
 }
