98a2e6abfb3444dd7a286b7c011bf9a4f9417892
Rockn26 <irfanank26@gmail.com>
2024-03-19 00:43:22 +0300

    change name2

--- src/main/java/dev/patika/VetClinic/core/config/WebConfig.java
+++ src/main/java/dev/patika/VetClinic/core/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://vet-app-full.vercel.app/")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
